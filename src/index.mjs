cc18e35e76c1f8b3f5ae37bc5709f3c6e5b27fd1
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2024-08-11 12:23:53 +0000

    arrow function

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,19 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+
+//Arrow Function
+//bisa buat anonymus function dengan cara ini :
+
+export default function (userName, message) {
+  console.log("anonymus function default");
+  return userName + message;
+}
+
+//arrow function
+(userName, message) => {
+  console.log("arrow function");
+  return userName + message;
+};
