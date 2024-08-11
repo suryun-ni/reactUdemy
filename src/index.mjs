44083eeb7309ba09dc94e41e1bc4cdefaca37afa
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2024-08-11 14:07:25 +0000

    quizArraySolve

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,13 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+
+function tranformToObjects(input) {
+  const tes = input.map((item) => ({ val: item }));
+  console.log(tes);
+  return tes;
+}
+
+tranformToObjects([1, 2, 3, 4, 5]);
