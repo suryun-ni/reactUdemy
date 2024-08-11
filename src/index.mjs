97f300a4c8a3d07be54ee053fa1762bb990d6727
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2024-08-11 13:46:14 +0000

    array

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,34 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+
+//Object : store key value
+//array store value only
+
+const hobbies = ["Sports", "Cooking", "Reading"];
+
+console.log(hobbies[1]);
+hobbies.push("working");
+console.log(hobbies);
+
+//findIndex , mencari item value bahkan yang baru dipush, mengembalikan true
+const index = hobbies.findIndex((item) => {
+  return item === "working";
+});
+//or write it like this
+const indexShort = hobbies.findIndex((item) => item === "Sports");
+
+console.log(index);
+console.log(indexShort);
+
+//Map function, transform item in array to other to another item
+//, mengembalikan array baru tanpa mengubah array aslinya
+
+const mapA = hobbies.map((item) => item + "!");
+console.log(mapA);
+
+//Map, ubah ke objek
+const mapB = hobbies.map((item) => ({ item: item }));
+console.log(mapB);
