dc30cc2fae3f37862f9622fc9de7cf5e148103c5
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2024-08-12 16:16:23 +0000

    destructuring material

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,35 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+
+//array destruc normally
+const userNameData = ["Max", "Test"];
+
+const firstName = userNameData[0];
+const lastName = userNameData[1];
+
+console.log("destruc normal firstName" + firstName);
+console.log("destruc normal lastName" + lastName);
+//array destruc more simple
+
+const [first, last] = ["ajo", "bajo"];
+console.log("destruc simple " + first);
+console.log("destruc simple " + last);
+
+//destruc object normally
+const user = {
+  name: "Max",
+  age: 34,
+};
+const name = user.name;
+const age = user.age;
+
+//destruc object simple
+const { nama: userName, umur } = {
+  nama: "tes",
+  umur: 34,
+};
+
+console.log(userName + " " + umur);
