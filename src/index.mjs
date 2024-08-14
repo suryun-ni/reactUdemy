e4e725946f342d98042c78bf5dd4578ea399c381
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2024-08-14 16:31:12 +0000

    refenrece

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,17 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+
+// example old/primitve value
+let userMessage = "Hello";
+userMessage = "Hello world"; //error karena gk ada tipe datanya
+// risk since you can change the value in it
+
+const hobbies = ["sports", "cooking"];
+
+hobbies.push("Working"); //karena fungsi push mengirim woriking ke array,
+// array tidak dapat dalam objek
+console.log(hobbies);
+// ******OBJECT IN JS SOCALLED REFRENCES VALLUE*******
