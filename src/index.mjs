a46da3c7e395da4267754099f46f22651efa98c9
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2024-08-14 15:50:43 +0000

    IFandOR

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,23 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+
+//IF Statement
+const password = prompt("your Password");
+
+if (password === "Hello") {
+  console.log("Hello works");
+} else if (password === "hello") {
+  console.log("hello works");
+} else {
+  console.log("access not Granted");
+}
+
+//LOOP
+const hobbies = ["Sports", "Cooking", "reading"];
+
+for (const hobby of hobbies) {
+  console.log(hobby);
+}
